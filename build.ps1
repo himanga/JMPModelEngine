855706eac7096f83c957b5f2ef666ac36f06f452
himanga <30961866+himanga@users.noreply.github.com>
2021-08-25 15:02:13 -0500

    Empty add-in

---
+++ build.ps1
@@ -0,0 +1,4 @@
+﻿$datepart = Get-Date -f yyyy-MM-dd_HH-mm-ss
+$fn = ".\JMPModelEngine $datepart"
+Compress-Archive -Path AddinFiles\* -DestinationPath $fn'.zip'
+Rename-Item $fn'.zip' $fn'.jmpaddin'
